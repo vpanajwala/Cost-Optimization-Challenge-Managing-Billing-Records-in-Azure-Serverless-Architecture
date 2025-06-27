cc7c82972e512933ae91b17a6a4cf1fe04e65722
vpanajwala <vpanajwala@gmail.com>
2025-06-27 20:43:10 +0530

    Update storage_setup.sh

--- Infra/storage_setup.sh
+++ Infra/storage_setup.sh
@@ -1,1 +1,11 @@
+#!/bin/bash
+az storage account create \
+  --name billingarchivestorage \
+  --resource-group myRG \
+  --location eastus \
+  --sku Standard_LRS
 
+az storage container create \
+  --name coldrecords \
+  --account-name billingarchivestorage \
+  --public-access off
